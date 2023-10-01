a5f20a3da66999b4602d276fae07ca5fe676aa8d
JP Carrascal <juanpcarrascal@gmail.com>
2023-10-01 22:57:05 +0200

    Added MoMc functionality

--- BLE-MIDI-Transparent01/BLE-MIDI-Transparent01.ino
+++ BLE-MIDI-Transparent01/BLE-MIDI-Transparent01.ino
@@ -1,15 +1,14 @@
 #include <Arduino.h>
 #include <BLEMidi.h>
+#include <Wire.h>
 #include <Button2.h>
-#include <Adafruit_NeoPixel.h>
+#include <HootBeat.h>
 
 #define POT_COUNT 4 // We have 4 potentiometers/knobs
 #define BUT_COUNT 4
-#define INTLED 22
-#define LONGCLICK_MS 5000
 
-#define LED_PIN 17
-#define NUMLEDS 2
+#define I2C_SDA 15
+#define I2C_SCL 2
 
 // Configurationflags:
 const bool debug = false;
@@ -17,31 +16,29 @@ const bool usbMIDI = true; // Send MIDI via USB?
 const bool sendRelease = true; // Send on release? 
 bool pickUpMode = true;
 bool BTconnected = false;
-static unsigned long last = 0;
 
 Button2 sw_1 = Button2(27, INPUT_PULLUP);
 Button2 sw_2 = Button2(18, INPUT_PULLUP);
 Button2 sw_3 = Button2(23, INPUT_PULLUP);
 Button2 sw_4 = Button2(19, INPUT_PULLUP);
 
-Adafruit_NeoPixel strip  = Adafruit_NeoPixel(NUMLEDS, LED_PIN);
+HootBeat hb = HootBeat(2, 17);
 
+const int intLed = 22;
 int count = 0;
-bool ledStatus = false, noteOnSent = false;
+bool ledStatus = false;
 const int CCchannel = 1;
 const int INchannel = 16;
 const int PCchannel = 13;
 const int note[BUT_COUNT] = {31,30,29,28};
 
 const int cc_pot[POT_COUNT] = {3, 4, 5, 6};
-const int pot[POT_COUNT] = {35,34,33,32};
+const int pot[POT_COUNT] = {34,35,33,32};
 int potval[POT_COUNT];
 int potvalIN[POT_COUNT];
 bool potPosCorrect[POT_COUNT] = {true, true, true, true};
-uint32_t color = 0xFF00FF;
-int colorCount = 0;
-int maxCount = 5;
-float inVel;
+const bool i2cMIDI = true;
+int anim = 5;
 
 void connected()
 {
@@ -49,23 +46,16 @@ void connected()
   BTconnected = true;
 }
 
-void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp)
-{
-  //Serial.printf("Received note on : channel %d, note %d, velocity %d (timestamp %dms)\n", channel, note, velocity, timestamp);
-  inVel = (float) velocity / 127;
-  colorCount = maxCount;
-}
-
 void setup() {
-  pinMode(INTLED, OUTPUT);
+  pinMode(intLed, OUTPUT);
   Serial.begin(115200);
+  if(i2cMIDI) Wire.begin(I2C_SDA, I2C_SCL);
   BLEMidiServer.begin("Transparent");
   BLEMidiServer.setOnConnectCallback(connected);
   BLEMidiServer.setOnDisconnectCallback([](){     // To show how to make a callback with a lambda function
     Serial.println("Disconnected");
     BTconnected = false;
   });
-  BLEMidiServer.setNoteOnCallback(onNoteOn);
   //BLEMidiServer.enableDebugging();
 
   for(int i=0; i<4; i++) {
@@ -75,7 +65,6 @@ void setup() {
 
   // Switch press and release callbacks
   sw_1.setPressedHandler(onButtonPressed);
-  //sw_1.setLongClickDetectedHandler(longClick);
   sw_2.setPressedHandler(onButtonPressed);
   sw_3.setPressedHandler(onButtonPressed);
   sw_4.setPressedHandler(onButtonPressed);
@@ -87,23 +76,16 @@ void setup() {
     sw_4.setReleasedHandler(onButtonReleased);  
   }
   analogReadResolution(10);
-
-  strip.begin();
-  strip.setBrightness(100);
-
-  for(uint8_t i=0; i<NUMLEDS; i++) {
-     strip.setPixelColor (i, 0x000000);
-  }
-  strip.show();
+  hb.setColor(0xFFFFFF);
+  anim = 5;
 }
 
 void loop() {
-
-  if(BTconnected) digitalWrite(INTLED, LOW);
+  if(BTconnected) digitalWrite(intLed, LOW);
   else {
     if(count >= 1000) {
       ledStatus = !ledStatus;
-      digitalWrite(INTLED, ledStatus);
+      digitalWrite(intLed, ledStatus);
       count = 0;
     }
     count++;
@@ -125,30 +107,17 @@ void loop() {
       ccSend(cc_pot[i], potvalNew[i], CCchannel);
     }
   }
-  /*--------- LEDs BEGIN ---------*/
-  if ( BLEMidiServer.isConnected() && (millis() - last >= 24) ) {
-    last += 24;
-    uint8_t  i;
-    for(i=0; i<NUMLEDS; i++) {
-      uint32_t c = 0;
-      float fade = ((float) colorCount / maxCount) * inVel;
-      fade *= fade;
-      if(colorCount > 0) {
-        c = dimColor(color, fade);
-        //Serial.println(fade);
-      }
-      strip.setPixelColor (i, c);
-    }
-    colorCount--;
-    strip.show();
-  }
-  /*--------- LEDs END ---------*/
-  
+  hb.step(anim);
 }
 
 void onButtonPressed(Button2& btn){
   int id = btn.getID();
   noteOnSend(note[id], 127, CCchannel);
+  if(id==0) hb.setColor(0x9900DD);
+  else if(id==1) hb.setColor(0xDDDDDD);
+  else if(id==2) hb.setColor(0xDDDD00);
+  else if(id==3) hb.setColor(0x0000FF);
+  hb.triggerFlash();
 }
 
 void onButtonReleased(Button2& btn){
@@ -161,6 +130,7 @@ void ccSend(int cc, int value, int channel) {
     debugThis("cc", cc, value);
   } else {
     BLEMidiServer.controlChange(channel, cc, value);
+    i2cSend("cc", cc, value);
   }
 }
 
@@ -169,6 +139,7 @@ void noteOnSend(int note, int vel, int channel) {
     debugThis("noteOn", note, vel);
   } else {
     BLEMidiServer.noteOn(channel, note, vel);
+    i2cSend("noteOn", note, vel);
   }
 }
 
@@ -177,14 +148,14 @@ void noteOffSend(int note, int vel, int channel) {
     debugThis("noteOff", note, vel);
   } else {
     BLEMidiServer.noteOff(channel, note, vel);
+    i2cSend("noteOff", note, vel);
   }
 }
 
 int mapAndClamp(int input) {
   int inMin = 1023;
   int inMax = 0;
-  // Invert (0-127) range if needed
-  int outval = map(input, inMin, inMax, 127, 0);
+  int outval = map(input, inMin, inMax, 0, 127);
   if(outval < 0) outval = 0;
   if(outval>127) outval = 127;
   return (outval);
@@ -198,6 +169,26 @@ int isPotCC(int val) {
   return (-1);
 }
 
+void i2cSend(String type, int val1, int val2) {
+  /*
+  int eventType = 0x09;
+  if(type == "noteOff") eventType = 0x08;
+  else if(type == "cc") eventType = 0x0B;
+  */
+  // Why these numbers???
+  int eventType = 117;
+  if(type == "noteOff") eventType = 109;
+  else if(type == "cc") eventType = 129;
+
+  if(i2cMIDI) {
+    Wire.beginTransmission(8);
+    Wire.write(eventType);
+    Wire.write(val1);
+    Wire.write(val2);
+    Wire.endTransmission(); 
+  }
+}
+
 void debugThis(String name, int i, int value) {
   if(debug) {
     Serial.print(name);
@@ -207,12 +198,4 @@ void debugThis(String name, int i, int value) {
     Serial.print(": ");
     Serial.println(value);
   }
-}
-
-uint32_t dimColor(uint32_t color, float fade) {
-    uint8_t r = fade * (float) ((color >> 16) & 0x0000FF);
-    uint8_t g = fade * (float) ((color >> 8) & 0x0000FF);
-    uint8_t b = fade * (float) (color & 0x0000FF);
-    uint32_t dimmedColor = (r<<16) + (g<<8) + (b);
-    return (dimmedColor);
 }
